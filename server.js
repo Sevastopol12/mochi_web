bf9dcbd5d8819cd1be33b5f2b9ef37647a5179bb
Sevastopol12 <chkalov253@gmail.com>
2025-05-14 15:09:29 +0700

    feat: define new routes for admin

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url';
 
 import { renderProductPage, listAll } from './controllers/ProductController.js';
 import { handleCommitOrder } from './controllers/OrderController.js';
+import { addProduct, displayRevenue } from './controllers/AdminController.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
@@ -20,8 +21,12 @@ app.set('views', path.join(__dirname, 'views'));
 // Routes
 app.get('/', async (req, res) => res.render('homepage', { title: 'Home page' }));
 app.get('/product', renderProductPage);
+app.get('/admin', async (req, res) => res.render('admin', {title: 'Admin page'}));
+
 app.get('/api/products', listAll);
-app.post('/api/order', handleCommitOrder)
+app.get('/api/revenue', displayRevenue);
+app.post('/api/order', handleCommitOrder);
+app.post('/api/add', addProduct);
 
 // Start server
 app.listen(3000, () =>
