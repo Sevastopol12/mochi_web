2cbc546cc61a4e00e3fae4fee96b26fb5645d194
Sevastopol12 <chkalov253@gmail.com>
2025-05-18 20:44:42 +0700

    Use process.env.PORT for Render deployment

--- server.js
+++ server.js
@@ -67,6 +67,7 @@ app.use('/api', (err, req, res, next) => {
 });
 
 // Start server
-// app.listen(3000, () =>
-//   console.log('Server running on http://localhost:3000')
-// );
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
